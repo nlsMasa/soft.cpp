ee1ce23328a3cb73feee193b95bdb6a1a3c4bf82
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-15 14:15:07 +0200

    [ecore][cpp] remove abstracteeimmutableelist and abstracteobjectelist

--- ecore/lib/src/ecore/impl/EObjectEList.hpp
+++ ecore/lib/src/ecore/impl/EObjectEList.hpp
@@ -12,59 +12,16 @@
 
 #include "ecore/ENotifyingList.hpp"
 #include "ecore/EUnsettableList.hpp"
-#include "ecore/impl/AbstractArrayEList.hpp"
-#include "ecore/impl/AbstractEObjectEList.hpp"
+#include "ecore/impl/AbstractENotifyingList.hpp"
 #include "ecore/impl/Proxy.hpp"
 
 namespace ecore::impl
 {
 
     template <typename T, bool containement = false, bool inverse = false, bool opposite = false, bool proxies = false, bool unset = false>
-    class EObjectEList : public AbstractEObjectEList<typename std::conditional<unset, EUnsettableList<T>, ENotifyingList<T>>::type,
-                                                     typename std::conditional<proxies, Proxy<T>, T>::type,
-                                                     containement,
-                                                     inverse,
-                                                     opposite>
+    class EObjectEList : public AbstractENotifyingList<typename std::conditional<unset, EUnsettableList<T>, ENotifyingList<T>>::type,
+                                                       typename std::conditional<proxies, Proxy<T>, T>::type>
     {
-        EObjectEList( const std::weak_ptr<EObject>& owner, int featureID, int inverseFeatureID, std::false_type )
-            : AbstractEObjectEList( owner, featureID, inverseFeatureID )
-        {
-        }
-
-        EObjectEList( const std::weak_ptr<EObject>& owner, int featureID, int inverseFeatureID, std::true_type )
-            : AbstractEObjectEList(
-                [=]( std::size_t index, const Proxy<T>& p ) -> T {
-                    auto oldObject = p.get();
-                    if( oldObject && oldObject->eIsProxy() )
-                    {
-                        auto o = owner.lock();
-                        if( o )
-                        {
-                            auto resolved = o->getInternal().eResolveProxy( oldObject );
-                            auto newObject = derived_pointer_cast<typename T::element_type>( resolved );
-                            if( newObject && newObject != oldObject )
-                            {
-                                p.setForced( newObject );
-                                std::shared_ptr<ENotificationChain> notifications;
-                                if constexpr(containement )
-                                {
-                                    notifications = inverseRemove( oldObject, notifications );
-                                    notifications = inverseAdd( newObject, notifications );
-                                }
-                                createAndDispatchNotification( notifications, ENotification::RESOLVE, oldObject, newObject, index );
-                                return newObject;
-                            }
-                        }
-                    }
-                    return oldObject;
-                },
-                [=]( std::size_t index, const T& v ) -> Proxy<T> { return Proxy<T>( v ); },
-                owner,
-                featureID,
-                inverseFeatureID )
-        {
-        }
-
     public:
         using EList<T>::add;
         using ENotifyingList<T>::add;
@@ -77,6 +34,142 @@ namespace ecore::impl
         virtual ~EObjectEList()
         {
         }
+
+        virtual std::shared_ptr<ENotifier> getNotifier() const
+        {
+            return owner_.lock();
+        }
+
+        virtual int getFeatureID() const
+        {
+            return featureID_;
+        }
+
+        virtual std::shared_ptr<EStructuralFeature> getFeature() const
+        {
+            auto owner = owner_.lock();
+            return owner ? owner->eClass()->getEStructuralFeature( featureID_ ) : nullptr;
+        }
+
+        virtual bool isSet() const
+        {
+            return isSet_;
+        }
+
+        virtual void unset()
+        {
+            clear();
+            bool oldIsSet = isSet_;
+            isSet_ = false;
+            createAndDispatchNotification( nullptr, ENotification::UNSET, oldIsSet, isSet_, -1 );
+        }
+
+    protected:
+        virtual void didChange()
+        {
+            isSet_ = true;
+        }
+
+        std::shared_ptr<AbstractNotification> createNotification( ENotification::EventType eventType,
+                                                                  const Any& oldValue,
+                                                                  const Any& newValue,
+                                                                  std::size_t position ) const
+        {
+            auto owner = owner_.lock();
+            return owner ? std::make_shared<Notification>( owner, eventType, featureID_, oldValue, newValue, position ) : nullptr;
+        }
+
+        virtual std::shared_ptr<ENotificationChain> inverseAdd( const T& object,
+                                                                const std::shared_ptr<ENotificationChain>& notifications ) const
+        {
+            if constexpr( inverse )
+            {
+                if constexpr( opposite )
+                    return object->getInternal().eInverseAdd( getOwner(), inverseFeatureID_, notifications );
+                else
+                    return object->getInternal().eInverseAdd( getOwner(), EOPPOSITE_FEATURE_BASE - featureID_, notifications );
+            }
+            else
+                return notifications;
+        }
+
+        virtual std::shared_ptr<ENotificationChain> inverseRemove( const T& object,
+                                                                   const std::shared_ptr<ENotificationChain>& notifications ) const
+        {
+            if constexpr( inverse )
+            {
+                if constexpr( opposite )
+                    return object->getInternal().eInverseRemove( getOwner(), inverseFeatureID_, notifications );
+                else
+                    return object->getInternal().eInverseRemove( getOwner(), EOPPOSITE_FEATURE_BASE - featureID_, notifications );
+            }
+            else
+                return notifications;
+        }
+
+    private:
+        EObjectEList( const std::weak_ptr<EObject>& owner, int featureID, int inverseFeatureID, std::false_type )
+            : AbstractENotifyingList()
+            , owner_(owner)
+            , featureID_( featureID )
+            , inverseFeatureID_( inverseFeatureID )
+            , isSet_(false)
+        {
+
+        }
+
+        EObjectEList( const std::weak_ptr<EObject>& owner, int featureID, int inverseFeatureID, std::true_type )
+            : AbstractENotifyingList( [=]( std::size_t index, const Proxy<T>& p ) -> T { return resolve( index, p ); },
+                                      [=]( std::size_t index, const T& t ) -> Proxy<T> { return proxify( index, t ); })
+            , owner_(owner)
+            , featureID_( featureID )
+            , inverseFeatureID_( inverseFeatureID )
+            , isSet_( false )
+        {
+        }
+
+        std::shared_ptr<EObject> getOwner() const
+        {
+            return owner_.lock();
+        }
+
+        T resolve( std::size_t index, const Proxy<T>& p )
+        {
+            auto oldObject = p.get();
+            if( oldObject && oldObject->eIsProxy() )
+            {
+                auto owner = getOwner();
+                if( owner )
+                {
+                    auto resolved = owner->getInternal().eResolveProxy( oldObject );
+                    auto newObject = derived_pointer_cast<typename T::element_type>( resolved );
+                    if( newObject && newObject != oldObject )
+                    {
+                        p.setForced( newObject );
+                        std::shared_ptr<ENotificationChain> notifications;
+                        if constexpr( containement )
+                        {
+                            notifications = inverseRemove( oldObject, notifications );
+                            notifications = inverseAdd( newObject, notifications );
+                        }
+                        createAndDispatchNotification( notifications, ENotification::RESOLVE, oldObject, newObject, index );
+                        return newObject;
+                    }
+                }
+            }
+            return oldObject;
+        }
+
+        Proxy<T> proxify( std::size_t index, const T& t )
+        {
+            return Proxy<T>( t );
+        }
+
+    private:
+        std::weak_ptr<EObject> owner_;
+        int featureID_;
+        int inverseFeatureID_;
+        bool isSet_;
     };
 } // namespace ecore::impl
 
