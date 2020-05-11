53f11229450c652bf1c02035fa7b7357038ff942
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-05-11 11:17:51 +0200

    [ecore][cpp] implements toAny methods to centralize conversion of EObject & EList<EObject> to Any

--- ecore/lib/src/ecore/AnyCast.hpp
+++ ecore/lib/src/ecore/AnyCast.hpp
@@ -11,21 +11,68 @@
 #define ECORE_ANYCAST_HPP_
 
 #include "ecore/Any.hpp"
-#include "ecore/EObject.hpp"
 #include "ecore/EList.hpp"
+#include "ecore/EObject.hpp"
 #include "ecore/SmartPtr.hpp"
+#include "ecore/impl/BasicEList.hpp"
 
 namespace ecore
 {
     template <typename T>
+    Any toAny( const T& t )
+    {
+        // Store an EObject
+        if constexpr( IsSharedEObject<T>::value )
+            return Any( std::static_pointer_cast<EObject>( t ) );
+        else
+            return Any( t );
+    }
+
+    template <typename T>
+    Any toAny( const Collection<T>& c )
+    {
+        if constexpr( IsSharedEObject<T>::value )
+        {
+            auto result = std::make_shared<impl::BasicEList<std::shared_ptr<EObject>>>();
+            std::transform( c.begin(), c.end(), result->end(), []( const T& t ) { return std::static_pointer_cast<EObject>( t ); } );
+            return std::static_pointer_cast<EList<std::shared_ptr<EObject>>>( result );
+        }
+        else
+        {
+            auto result = std::make_shared<impl::BasicEList<Any>>();
+            std::transform( c.begin(), c.end(), result->end(), []( const T& t ) { return Any( t ); } );
+            return std::static_pointer_cast<EList<Any>>( result );
+        }
+    }
+
+    template <typename T>
+    Any toAny( const std::shared_ptr<EList<T>>& l )
+    {
+        if constexpr( IsSharedEObject<T>::value )
+        {
+            // Store a list of EObject
+            if constexpr( std::is_same<EObject, typename T::element_type>::value )
+                return l;
+            else
+                return l->asEListOf<std::shared_ptr<EObject>>();
+        }
+        else
+            // Store a list of Any
+            return l->asEListOf<Any>();
+    }
+
+    template <typename T>
     T anyObjectCast( const Any& any )
     {
         auto id = &any.type();
+        // Any store an EObject
         if( id == &typeid( std::shared_ptr<EObject> ) )
         {
             auto object = anyCast<std::shared_ptr<EObject>>( any );
             return derived_pointer_cast<typename T::element_type>( object );
-        }   
+        }
+
+        // Any store the correct Type
         if( id == &typeid( T ) )
             return anyCast<T>( any );
         return nullptr;
@@ -35,22 +82,41 @@ namespace ecore
     std::shared_ptr<EList<T>> anyListCast( const Any& any )
     {
         auto id = &any.type();
+        
+        // Any store a list of EObject
+        if( id == &typeid( std::shared_ptr<EList<std::shared_ptr<EObject>>> ) )
+        {
+            auto l = anyCast<std::shared_ptr<EList<std::shared_ptr<EObject>>>>( any );
+            if constexpr( is_shared_ptr<T>::value && std::is_same<EObject, typename T::element_type>::value )
+                return l;
+            else
+                return l->asEListOf<T>();
+        }
+
+        // Any store a list of EObject with the correct type
+        if( id == &typeid( std::shared_ptr<EList<T>> ) )
+            return anyCast<std::shared_ptr<EList<T>>>( any );
+
+        // Any store a list of Any
         if( id == &typeid( std::shared_ptr<EList<Any>> ) )
         {
             auto l = anyCast<std::shared_ptr<EList<Any>>>( any );
-            return l->asEListOf<T>();
-        }
-        if( id == &typeid( std::shared_ptr<EList<std::shared_ptr<EObject>>> ) )
-        {
+            if constexpr( std::is_same<Any, T>::value )
+                return l;
+            
             if constexpr( IsSharedEObject<T>::value )
             {
-                auto l = anyCast<std::shared_ptr<EList<std::shared_ptr<EObject>>>>( any );
-                return l->asEListOf<T>();
+                auto o = l->asEListOf<std::shared_ptr<EObject>>();
+                if constexpr( std::is_same<EObject, typename T::element_type>::value )
+                    return o;
+                else
+                    return o->asEListOf<T>();
             }
+            
+            return l->asEListOf<T>();
         }
-        if( id == &typeid( std::shared_ptr<EList<T>> ) )
-            return anyCast<std::shared_ptr<EList<T>>>( any );
         return nullptr;
+        
     }
 
 } // namespace ecore
