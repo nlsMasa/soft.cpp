4af4a3a851c30572f3cf959b9791b05b7491d434
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-05-15 12:36:06 +0200

    [ecore][cpp] define EResourceIDManager class

---
+++ ecore/lib/src/ecore/EResourceIDManager.hpp
@@ -0,0 +1,36 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+
+#ifndef ECORE_ERESOURCEIDMANAGER_HPP_
+#define ECORE_ERESOURCEIDMANAGER_HPP_
+
+#include <memory>
+#include <string>
+
+namespace ecore
+{
+    class EObject;
+
+    class EResourceIDManager
+    {
+    public:
+        virtual ~EResourceIDManager() = default;
+
+        virtual void registerObject( const std::shared_ptr<EObject>& eObject ) = 0;
+
+        virtual void unregisterObject( const std::shared_ptr<EObject>& eObject ) = 0;
+
+        virtual std::string getID( const std::shared_ptr<EObject>& eObject ) const = 0;
+
+        virtual std::shared_ptr<EObject> getEObject( const std::string& ) const = 0;
+    };
+
+}
+
+#endif
